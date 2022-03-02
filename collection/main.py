9473aa378e70deaf8c1d48ad490f1c64f97478d6
owlia <mohamadhasanolia>
2022-03-01 22:19:49 -0500

    collection grpc running

--- collection/main.py
+++ collection/main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+import grpc
+
+import proto.coordinate_pb2 as coordinate_pb2
+import proto.coordinate_pb2_grpc as coordinate_pb2_grpc
+
+channel = grpc.insecure_channel("localhost:50051")
+stub = coordinate_pb2_grpc.CoordinateStub(channel)
+response = stub.SendCoordination(coordinate_pb2.CoordinateRequest(x=1, y=2))
+print(response.message)
