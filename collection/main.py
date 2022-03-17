db7b4d66eda2eb245aeca4e81f00880401bcda3e
kamyar <kamyar.abhari@ezra.com>
2022-03-17 01:18:33 -0400

    added client streaming

--- collection/main.py
+++ collection/main.py
@@ -3,7 +3,33 @@ import grpc
 import proto.coordinate_pb2 as coordinate_pb2
 import proto.coordinate_pb2_grpc as coordinate_pb2_grpc
 
-channel = grpc.insecure_channel("localhost:50051")
-stub = coordinate_pb2_grpc.CoordinateStub(channel)
-response = stub.SendCoordination(coordinate_pb2.CoordinateRequest(x=1, y=2))
-print(response.message)
+
+def make_coordinate(x, y):
+    return coordinate_pb2.CoordinateRequest(x=x, y=y)
+
+
+def generate_messages():
+    messages = [
+        make_coordinate(1, 2),
+        make_coordinate(3, 4),
+        make_coordinate(5, 6),
+        make_coordinate(7, 8),
+        make_coordinate(9, 10),
+    ]
+    for msg in messages:
+        print("Sending %s at %s" % (msg.x, msg.y))
+        yield msg
+
+
+def send_message(stub):
+    response = stub.SendCoordination(generate_messages())
+    print(response)
+
+
+def run():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = coordinate_pb2_grpc.CoordinateStub(channel)
+        send_message(stub)
+
+
+run()
