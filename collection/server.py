7ec928f8109f29a322e6897cd4d51c4240adcd67
kamyar <kamyar.abhari@ezra.com>
2022-03-19 01:07:29 -0400

    Added timestamp: The client timestamps the coordinate and sends them over to the server

--- collection/server.py
+++ collection/server.py
@@ -5,12 +5,22 @@ import proto.coordinate_pb2 as coordinate_pb2
 import proto.coordinate_pb2_grpc as coordinate_pb2_grpc
 
 import logging
+from datetime import datetime
 
 
 class CoordinateServicer(coordinate_pb2_grpc.CoordinateServicer):
     def SendCoordination(self, request_iterator, context):
         for coordinate in request_iterator:
-            log.info("got coordinates:%f,%f" % (coordinate.x, coordinate.y))
+            log.info(
+                "got coordinates: (%f,%f) at %s"
+                % (
+                    coordinate.x,
+                    coordinate.y,
+                    datetime.fromtimestamp(
+                        coordinate.t.seconds + coordinate.t.nanos / 1e9
+                    ),
+                )
+            )
         return coordinate_pb2.CoordinateResponse(
             message="Client recieved all coordinates"
         )
