264589d665b5afad36e5b7240786c649ea1c5d55
Mohammad Owlia <mohammad@joinsherpa.com>
2022-07-18 20:04:56 -0400

    buffer data save to db

--- visualization/bg/Data/Models/coord.model.ts
+++ visualization/bg/Data/Models/coord.model.ts
@@ -1,20 +1,29 @@
-import { Schema, model, ObjectId } from "mongoose";
+import { Schema, model, ObjectId, Types } from "mongoose";
 
 // 1. Create an interface representing a document in MongoDB
 export interface ICoordinate {
-  x_coordinate: Number;
-  y_coordinate: Number;
-  t_coordinate: Date;
+  x: Number;
+  y: Number;
+  t: Date;
+}
+export interface ICoordinates {
+  data: Types.DocumentArray<ICoordinate>;
+  t_start: Date;
   session_id: ObjectId;
 }
 
 // 2. Create a Schema corresponding to the document interface
 const coordSchema = new Schema<ICoordinate>({
-  x_coordinate: { type: Schema.Types.Number, required: true },
-  y_coordinate: { type: Schema.Types.Number, required: true },
-  t_coordinate: { type: Schema.Types.Date, required: true },
+  x: { type: Schema.Types.Number, required: true },
+  y: { type: Schema.Types.Number, required: true },
+  t: { type: Schema.Types.Date, required: true },
+});
+
+const coordsSchema = new Schema<ICoordinates>({
+  data: { type: Schema.Types.Array, required: true },
+  t_start: { type: Schema.Types.Date, required: true },
   session_id: { type: Schema.Types.ObjectId, ref: "SessionModel" },
 });
 
 // 3. Create a Model
-export default model<ICoordinate>("CoordinateModel", coordSchema);
+export default model<ICoordinates>("CoordinatesModel", coordsSchema);
