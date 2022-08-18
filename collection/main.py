c1517b49961c3fab564467d268c1c4cc9636a51b
Mohammad Owlia <mohammad@joinsherpa.com>
2022-08-18 17:17:29 -0400

    cleanup python

--- collection/main.py
+++ collection/main.py
@@ -1,6 +1,5 @@
 import grpc
 import random
-import math
 import logging
 import datetime
 import os
@@ -10,24 +9,13 @@ import proto.coordinate_pb2_grpc as coordinate_pb2_grpc
 from google.protobuf.timestamp_pb2 import Timestamp
 
 from dotenv import load_dotenv
+
+from mock_data_generator import mock_data_generator
 load_dotenv()  # take environment variables from .env.
 GRPC_HOST=os.getenv('GRPC_HOST')
 MAX_DATA_RATE = 3000 # In Hz
 
-def generate_random_pnt_circle(radius, center_x, center_y, alpha):
-    """This function generates a random point inside a circle
-
-    Args:
-        radius (int): radius of the circle
-        center_x (int): x-coordinate of circle's center point
-        center_y (int): y-coordinate of circle's center point
-
-    Returns:
-        int, int: a random point inside a circle
-    """
-    # calculate and return coordinates
-    return radius * math.cos(alpha) + center_x, radius * math.sin(alpha) + center_y
-
+mock_data = mock_data_generator()
 
 def stream_coordinations_logic():
     """This function generates messages (iterator) be sent to the server from the stub (client).
@@ -39,25 +27,32 @@ def stream_coordinations_logic():
     # We are making use of protobuf's Timestamp() to transfer timestamps
     timestamp = Timestamp()
 
-    alpha=0
-    r=0
     count=0
     lastHzCalculate=datetime.datetime.now()
     lastTime=datetime.datetime.now()
 
     # Inifinite loop to send data from stub (client) to the server
     while True:
+
+        # Display the Actual Data rate every second
         count += 1
-        alpha += 0.001
-        r += 0.0001
+        if count == MAX_DATA_RATE:
+            now = datetime.datetime.now()
+            log.info(f"Outgoing GRPC rate is {round( MAX_DATA_RATE/(now - lastHzCalculate).total_seconds())}Hz")
+            count = 0
+            lastHzCalculate = now
+
         # stamp the data with current date + time
         timestamp.FromDatetime(datetime.datetime.now())
+
+        # SEND COORDINATION HERE --------------------------------------------------------------
         # construct the message
         msg = coordinate_pb2.StreamCoordinateRequest(
-            x=generate_random_pnt_circle(10* math.sin(r), 0, 0, alpha)[0],
-            y=generate_random_pnt_circle(10 * math.sin(r), 0, 0, alpha)[1],
+            x=mock_data.iterate()[0],
+            y=mock_data.iterate()[1],
             t=timestamp,
         )
+        # SEND COORDINATION HERE --------------------------------------------------------------
 
         # Throttle the data sent to 
         while (datetime.datetime.now() - lastTime) < datetime.timedelta(microseconds=1000000 / MAX_DATA_RATE):
@@ -65,45 +60,36 @@ def stream_coordinations_logic():
 
         lastTime=datetime.datetime.now()
 
-        # Display the Actual Data rate every second
-        if count == MAX_DATA_RATE:
-            now = datetime.datetime.now()
-            log.info(f"Outgoing GRPC rate is {round( MAX_DATA_RATE/(now - lastHzCalculate).total_seconds())}Hz")
-            count = 0
-            lastHzCalculate = now
-
-
-        #log.info(
-        #    f"Sending ({msg.x}, {msg.y}) at {datetime.datetime.fromtimestamp(msg.t.seconds + msg.t.nanos / 1e9)}"
-        #)
         yield msg
-
-
-calibrated_quads = [False, False,  False,  False]
-
-def is_calibrated():
-    return calibrated_quads[0] and calibrated_quads[1] and calibrated_quads[2] and calibrated_quads[3]
 
 def generate_calibration_stream():
     while not is_calibrated():
         yield coordinate_pb2.CalibrateRequest()
+    
+# CALIBRATION PARAMETERS AND END CONDITION --------------------------------------------------------------
+calibrated_quads = [False, False,  False,  False]
+def is_calibrated():
+    return calibrated_quads[0] and calibrated_quads[1] and calibrated_quads[2] and calibrated_quads[3]
+# CALIBRATION PARAMETERS AND END CONDITION --------------------------------------------------------------
+
 
 def run():
     """Create  the client"""
     with grpc.insecure_channel(GRPC_HOST) as channel:
         stub = coordinate_pb2_grpc.CoordinateStub(channel)
         
-        log.info("Calibration Initialized")
 
-        # Calibration Starts
+        log.info("Calibration Initialized")
+        # CALIBRATION LOGIC --------------------------------------------------------------
         for calibrationResponse in stub.calibrate(generate_calibration_stream()):
             if calibrationResponse.quad !=-1:
                 calibrated_quads[calibrationResponse.quad - 1] = True
                 log.info(f"Calibration received for quad {calibrationResponse.quad}") 
             else:
                 log.info("Waiting")
-
+        # CALIBRATION LOGIC --------------------------------------------------------------
         log.info("Calibration Finalized") 
+
 
         stub.streamCoordinate(stream_coordinations_logic())
 
